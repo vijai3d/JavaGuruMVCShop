a9c7702f9aed49044378b9f6776c7b8b93f20fc3
Vijai3D <viktor.antipin@gmail.com>
2017-04-19 22:52:08 +0300

    nav bar ready

---
+++ src/main/java/com/javaguru/JavaguruMvcShopApplication.java
@@ -0,0 +1,12 @@
+package com.javaguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaguruMvcShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaguruMvcShopApplication.class, args);
+	}
+}
