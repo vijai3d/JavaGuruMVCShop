5b7bac9c006f4a04b5b0a5f3c2a59f02953c8401
Vijai3D <viktor.antipin@gmail.com>
2017-04-22 15:04:01 +0300

    nav bar ready

---
+++ src/test/java/com/javaguru/controller/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package com.javaguru.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vijai3D on 19.04.2017.
+ */
+public class HomeControllerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void index() throws Exception {
+    }
+
+}
